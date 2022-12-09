eac3ea486279582b4936fe01e6b0a6572a56ac1b
ivangrinko-dev <grinko.work.1982@mail.ru>
2022-12-09 18:54:49 +0300

    dop

--- Namber/19/app.js
+++ Namber/19/app.js
@@ -1,4 +1,25 @@
 // Даны переменные a = 10 и b = 3. Найдите остаток от деления a на b
-const a = 10;
-const b = 3;
-console.log(a % b);
+const a = +prompt();
+const b = +prompt();
+const c = +prompt();
+
+let value = b * b - 4 * a * c;
+let x1 = (-b - Math.sqrt(value)) / (2 * a);
+let x2 = (-b + Math.sqrt(value)) / (2 * a);
+
+if (value > 0) {
+    console.log((-b - Math.sqrt(value)) / (2 * a));   
+    console.log((-b + Math.sqrt(value)) / (2 * a));
+
+} else if (value === 0) {
+    console.log(-b / (2 * a));
+
+} else if (value < 0) {
+    console.log('корней нет');
+}
+
+if (x1 > x2) {
+    console.log(x2);
+} else if(x1 < x2) {
+    console.log(x1);
+}
