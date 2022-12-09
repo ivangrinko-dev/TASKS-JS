e9a27d7b9ab346d4b5c374d4caccff4b184c92cc
vanyaolyagrinko-work <vanya.grinko.82@mail.ru>
2022-12-09 12:50:27 +0300

    15

--- Namber/14/app.js
+++ Namber/14/app.js
@@ -1,4 +1,8 @@
-// Даны переменные a = 10 и b = 3. Найдите остаток от деления a на b
-const a = 10;
-const b = 3;
-console.log(a % b);
+// Вы вводите значение с клавиатуры. Если значение четное, то вывести true, в
+// противном случае false
+const a = +prompt();
+if (a % 2 === 0) {
+    console.log('true')
+} else if (a % 2>0) {
+    console.log('false')
+} 
