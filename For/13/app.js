68dc82baf258bddbb16ac211ab5636b458596fcb
ivangrinko-dev <grinko.work.1982@mail.ru>
2022-12-20 22:41:56 +0300

    slip

--- For/13/app.js
+++ For/13/app.js
@@ -3,3 +3,22 @@
 // anna -> true
 // мем -> true
 // тест -> false
+
+const a = `anna`;
+
+// // if (a.startsWith() === a.endsWith()) {
+// //     console.log(true)
+// // } else {
+// //     console.log(false)
+// // }
+// console.log(a.startsWith())
+
+//let a = b.length - 1;
+for (let i = 0; i < a.length / 2; i++) {
+  if (a[i] !== a[lastIndex - i]) {
+    return false;
+  }
+  {
+    return true;
+  }
+}
--- For/9/app.js
+++ For/9/app.js
@@ -1,10 +1,16 @@
 // На вход число. Необходимо вывести все числа от 1 до введенного числа. Добавить
 // проверку вводимого значения, если это текст –> сообщение об 
 
-const a = 9;
+const a = 5;
 
 let result = ``;
+
+
 for (let i = 1; i <= a; i++) {
-  result += i;
-}
-console.log(result)
+  if (isNaN(a)) {
+    console.log(`Ошибка ввода`)
+  } else {
+      result += i;
+    }
+  }
+  console.log(result)
