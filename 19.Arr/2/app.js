ba29b9ebd036a5a4b3a536516026f6f6fe088691
vanyaolyagrinko-work <vanya.grinko.82@mail.ru>
2022-12-30 13:54:43 +0300

    edy

--- 19.Arr/2/app.js
+++ 19.Arr/2/app.js
@@ -2,15 +2,26 @@
 
 const a = [1, 2, 3, 4, 5];
 
-for (let i = 0; i < a.length; i++) {
-  console.log(a[i]);
+for (let i = 1; i <= a.length; i++) {
+  console.log(i)
 }
 
-for (let i of a) {
-  console.log(i);
-}
-let i = 0;
-do {
-  console.log(a[i]);
+let i = 1;
+while (i <= a.length) {
+  console.log(i)
   i++
-} while (i < a.length);
+}
+
+let x = 1;
+do {
+  console.log(x)
+  x++
+} while (i <= a.length)
+
+for (let i of a) {
+  console.log(i)
+}
+
+for (let i in a) {
+  console.log(`a.${i} = ${a[i]}`)
+}
