a497684cc066045b160f7d39d7678c4dc2f6e058
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-01-05 22:10:57 +0300

    Решение задач

--- 020. Methods_arr/7/app.js
+++ 020. Methods_arr/7/app.js
@@ -1,1 +1,6 @@
-// 
+// 7. На входе задан массив [‘hschool’, ‘hschool_0’, ‘hschool_1’]. Напишите все возможные
+// способы обращения к первому и последнему элементам
+
+const arr = [‘hschool’, ‘hschool_0’, ‘hschool_1’]
+
+console.log(arr)
--- 020. Methods_arr/8/app.js
+++ 020. Methods_arr/8/app.js
@@ -1,1 +1,32 @@
-// 
+// 8. На входе n – количество элементов массива. Далее производится заполнение
+// массива с клавиатуры. Если все элементы массива – числа, то вывести логическое
+// true, в противном случае false. Every, forEach
+
+const n = prompt();
+const arr = [];
+
+for (let i = 0; i < n; i++) {
+    arr.push(prompt());
+  }
+
+let result = arr.every(function(elem){
+    if (!isNaN(elem)){
+        return true
+    } else {
+        return false
+    }
+})
+
+console.log(result)
+
+let bool = false;
+
+arr.forEach(function(elem){
+    if (!isNaN(elem)) {
+        bool = true;
+        return null;
+    } else {
+        bool = false;
+    }
+})
+console.log(bool);
